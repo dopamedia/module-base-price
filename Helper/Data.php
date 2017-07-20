607021030177fc88e799e79ca6fbb1fb1fed50ec
Andreas Penz <office@dopa.media>
2017-07-20 10:33:40 +0200

    fix bug where unit gets not resolved correctly

--- Helper/Data.php
+++ Helper/Data.php
@@ -45,10 +45,24 @@ class Data extends \Magento\Framework\Url\Helper\Data
     protected function getConvertedAmount(\Magento\Framework\Pricing\SaleableInterface $saleableItem)
     {
         return $this->unitConverter->convert(
-            $saleableItem->getData(ProductAttributeInterface::CODE_BASE_PRICE_PRODUCT_UNIT),
-            $saleableItem->getData(ProductAttributeInterface::CODE_BASE_PRICE_REFERENCE_UNIT),
+            $this->getProductUnit($saleableItem)->getCode(),
+            $this->getReferenceUnit($saleableItem)->getCode(),
             $saleableItem->getData(ProductAttributeInterface::CODE_BASE_PRICE_PRODUCT_AMOUNT)
         );
+    }
+
+    /**
+     * @param \Magento\Framework\Pricing\SaleableInterface|\Magento\Catalog\Model\Product $saleableItem
+     * @param string $attributeCode
+     * @return string
+     */
+    private function getUnitFromAttributeText(
+        \Magento\Framework\Pricing\SaleableInterface $saleableItem,
+        string $attributeCode
+    ): string
+    {
+        $unit = $saleableItem->getAttributeText($attributeCode);
+        return strtoupper(str_replace(' ', '_', $unit));
     }
 
     /**
@@ -66,9 +80,34 @@ class Data extends \Magento\Framework\Url\Helper\Data
      * @param \Magento\Framework\Pricing\SaleableInterface|\Magento\Catalog\Model\Product $saleableItem
      * @return UnitInterface
      */
+    public function getProductUnit(\Magento\Framework\Pricing\SaleableInterface $saleableItem)
+    {
+        $productUnitCode = $saleableItem->getData(ProductAttributeInterface::CODE_BASE_PRICE_PRODUCT_UNIT);
+
+        if (!$this->unitConverter->checkIfUnitExists($productUnitCode)) {
+            $productUnitCode = $this->getUnitFromAttributeText(
+                $saleableItem, ProductAttributeInterface::CODE_BASE_PRICE_PRODUCT_UNIT
+            );
+        }
+
+        return $this->measureBuilder->getUnit($productUnitCode);
+    }
+
+
+    /**
+     * @param \Magento\Framework\Pricing\SaleableInterface|\Magento\Catalog\Model\Product $saleableItem
+     * @return UnitInterface
+     */
     public function getReferenceUnit(\Magento\Framework\Pricing\SaleableInterface $saleableItem)
     {
         $referenceUnitCode = $saleableItem->getData(ProductAttributeInterface::CODE_BASE_PRICE_REFERENCE_UNIT);
+
+        if (!$this->unitConverter->checkIfUnitExists($referenceUnitCode)) {
+            $referenceUnitCode = $this->getUnitFromAttributeText(
+                $saleableItem, ProductAttributeInterface::CODE_BASE_PRICE_REFERENCE_UNIT
+            );
+        }
+
         return $this->measureBuilder->getUnit($referenceUnitCode);
     }
 }
